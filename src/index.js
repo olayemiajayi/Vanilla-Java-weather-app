e700577e6a85b3af4c8c8f84a15378d0d2f1cb4d
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-03-08 20:47:17 -0700

    updated the weather forecast in the javascript

--- src/index.js
+++ src/index.js
@@ -19,34 +19,37 @@ console.log(date.getHours());
 city.innerHTML = `${day}, ${hour}:${minute}`;
 
 //search engine and current weather
+
 function displayWeather(response) {
   console.log(response);
-  console.log(response.data.main.temp);
-  console.log(response.data.main.humidity);
+  console.log(response.data.temperature.current);
+  console.log(response.data.temperature.humidity);
   console.log(response.data.wind.speed);
-  console.log(response.data.weather[0].description);
-  console.log(response.data.weather[0].icon);
+  console.log(response.data.condition.description);
+  console.log(response.data.condition.icon);
   windTemp = Math.round(response.data.wind.speed);
 
   let temperature = document.querySelector("#temperature-app");
-  temperature.innerHTML = Math.round(response.data.main.temp);
+  temperature.innerHTML = Math.round(response.data.temperature.current);
   let humidity = document.querySelector("#humidity-weather");
-  humidity.innerHTML = `Humidity:${response.data.main.humidity}`;
+  humidity.innerHTML = `Humidity:${response.data.temperature.humidity}`;
   let wind = document.querySelector("#wind-weather");
-  wind.innerHTML = `Wind: ${windTemp}Km/h`;
+  wind.innerHTML = `Wind:${windTemp}Km/h`;
   let description = document.querySelector("#description-weather");
-  description.innerHTML = response.data.weather[0].description;
+  description.innerHTML = response.data.condition.description;
   let icon = document.querySelector("#icon-weather");
   icon.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
 }
+
 function search(city) {
-  let apiKey = "8944afa6845bd7c413a687258d3211ef";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8944afa6845bd7c413a687258d3211ef&units=metric`;
+  let apiKey = "308ee4b586fftbc5ce47ob29fd3f7a87";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=308ee4b586fftbc5ce47ob29fd3f7a87`;
   axios.get(apiUrl).then(displayWeather);
 }
+
 function townShip(event) {
   event.preventDefault();
   let weather = document.querySelector("#input-form");
